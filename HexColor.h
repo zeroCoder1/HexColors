f8192b32d9f04ac11a493acd00d68f706f019ff2
shrutesh1@gmail.com <shrutesh1@gmail.com>
2013-02-01 20:00:32 +0530

    first commit

---
+++ HexColor.h
@@ -0,0 +1,18 @@
+//
+//  HexColor.h
+//  HexColor
+//
+//  Created by Shrutesh Sharma on 01/01/13.
+//  Copyright (c) 2013 Shrutesh Sharma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface HexColor : NSObject
+
+
+
++ (UIColor *) colorWithHex: (NSString *) hex;
+
+@end
